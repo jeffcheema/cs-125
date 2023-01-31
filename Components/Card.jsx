aef10282b5233041b068e560fc17fcb3eaca1938
Jeff Cheema <jeffcheema@Jeffs-MacBook-Air-2.local>
2023-01-31 14:42:39 -0800

    reorganized

---
+++ Components/Card.jsx
@@ -0,0 +1,27 @@
+const CreateCard = () => {
+  return (
+    <Card style={Styles.container}>
+      <Card.Content>
+        <Title>Geeks For Geeks</Title>
+      </Card.Content>
+      <Card.Cover
+        source={{
+          uri: "https://media.geeksforgeeks.org/wp-content/uploads/20220217151648/download3-200x200.png",
+        }}
+      />
+      <Card.Content>
+        <Paragraph>A Computer Science portal for Geeks</Paragraph>
+      </Card.Content>
+      <Card.Actions>
+        <Button title="Add to Favourites"></Button>
+      </Card.Actions>
+    </Card>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    alignContent: "center",
+    margin: 37,
+  },
+});
