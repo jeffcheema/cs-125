b36d542d06305d2db4908e51b352d612e6f48c38
Mingjia Wang <mingjia@Mingjias-MacBook-Pro.local>
2023-02-03 23:42:43 -0800

    Added axios call with headers

--- Views/Dashboard.jsx
+++ Views/Dashboard.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { StyleSheet, View } from "react-native";
 import CreateCard from "../Components/Card";
 
 const Dashboard = () => {
+  [movies, setMovies] = useState([]);
+
+  useEffect(async () => {
+    const config = {
+      headers: {
+        'client': 'MOVI_162',
+        'x-api-key': 'Y38wpjAaP566ppQwom80J207PBbha4Da2neKmR5S',
+        'authorization': 'Basic TU9WSV8xNjJfWFg6eDFjbndocWttVXFx',
+        'territory': 'XX',
+        'api-version': 'v200',
+        'geolocation': '-22.0; 14.0',
+        'device-datetime': '2023-02-04T07:11:53.637Z'
+      }
+    }
+
+    axios.get('https://api-gate2.movieglu.com/filmsNowShowing/?n=10', config).then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <View style={styles.container} styler>
       <CreateCard />
+      <Text>{movies}</Text>
     </View>
   );
 };
