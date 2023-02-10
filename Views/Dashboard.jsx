3725d77b3ff68798dd4e9a4c54751ca3eb2b72bd
claudeisakeeb <claudeyan2001@gmail.com>
2023-02-10 11:05:08 -0800

    Movie dashboard

--- Views/Dashboard.jsx
+++ Views/Dashboard.jsx
@@ -1,59 +1,60 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { StyleSheet, View, Text } from "react-native";
-import CreateCard from "../Components/Card";
+import axios from "axios";
+import { SafeAreaView, StyleSheet, Text, FlatList } from "react-native";
+import MovieCard from "../Components/MovieCard";
 
 const Dashboard = () => {
-  [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
-
-    // console.log('')
-
     const config = {
       params: {
-        n: 2
+        n: 2,
       },
       headers: {
-        'client': 'MOVI_162',
-        'x-api-key': 'Y38wpjAaP566ppQwom80J207PBbha4Da2neKmR5S',
-        'authorization': 'Basic TU9WSV8xNjJfWFg6eDFjbndocWttVXFx',
-        'territory': 'XX',
-        'api-version': 'v200',
-        'geolocation': '-22.0; 14.0',
-        'device-datetime': (new Date()).toISOString()
-      }
-    }
-    console.log("Line 27");
-    axios.get('https://api-gate2.movieglu.com/filmsNowShowing', config).then((response) => {
-      if (response) {
-        console.log(response?.data?.films);
-        setMovies(response?.data?.films);
-      } 
-    
-    });
+        client: "MOVI_162",
+        "x-api-key": "Y38wpjAaP566ppQwom80J207PBbha4Da2neKmR5S",
+        authorization: "Basic TU9WSV8xNjJfWFg6eDFjbndocWttVXFx",
+        territory: "XX",
+        "api-version": "v200",
+        geolocation: "-22.0; 14.0",
+        "device-datetime": new Date().toISOString(),
+      },
+    };
+    axios
+      .get("https://api-gate2.movieglu.com/filmsNowShowing", config)
+      .then((response) => {
+        if (response) {
+          setMovies(response?.data?.films);
+        }
+      });
   };
 
   useEffect(() => {
     return () => {
-      const movies = getMovies();
-      
-    }
+      getMovies();
+    };
   }, []);
 
   return (
-    <View style={styles.container} styler>
-      {/* <CreateCard /> */}
-      {movies?.map(({film_name}, index)=>{
-       <>
-       <Text>Movie #{index}</Text>
-       <Text>{film_name}</Text>
-
-       </>
-       
-
-      })}
-    </View>
+    <SafeAreaView style={styles.container}>
+      {movies && (
+        <FlatList
+          data={movies}
+          renderItem={({ index, item }) => {
+            return (
+              <MovieCard
+                id={index}
+                title={item.film_name}
+                uri={item.images.still[1].medium.film_image}
+                synopsis={item.synopsis_long}
+              />
+            );
+          }}
+          keyExtractor={(item) => item.index}
+        />
+      )}
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
