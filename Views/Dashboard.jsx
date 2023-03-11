516f3493f2da007568472220ace24857f162ca08
Mingjia Wang <mingjiawangdev@gmail.com>
2023-03-11 11:16:26 -0800

    Added weather filtering festure

--- Views/Dashboard.jsx
+++ Views/Dashboard.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { SafeAreaView, StyleSheet, Text, FlatList } from "react-native";
 import MovieCard from "../Components/MovieCard";
+import { useAuth } from "../Providers/AuthProvider";
 
 const Dashboard = () => {
   const [movies, setMovies] = useState([]);
+  const { currentUser } = useAuth;
 
   const getGenres = async (id) => {
     const options = {
@@ -26,10 +28,41 @@ const Dashboard = () => {
     });
   };
 
+  const filterByWeather = async () => {
+    temperature = await axios.get('http://api.weatherapi.com/v1', {
+      key: '7c0d315f79674727806182811231103',
+      q: 92617
+    });
+
+    movies.filter(movie => {
+      if (temperature < 60) {
+        if (!currentUser.weatherPreference && movie.rating !== 'PG-13' && movie.rating !== 'R') {
+          return true;
+        } else {
+          return false;
+        }
+      } else if (!temperature) {
+        if (!currentUser.weatherPreference && movie.rating == 'PG-13' && movie.rating == 'R') {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    });
+    console.log('movies after weather filtering:');
+    console.log(movies);
+  };
+
+  const filterByGenre = () => {
+
+  };
+
   const getMovies = async () => {
     const config = {
       params: {
-        n: 4,
+        n: 10,
       },
       headers: {
         client: "MOVI_162",
@@ -46,8 +79,11 @@ const Dashboard = () => {
       .then((response) => {
         if (response) {
           console.log('movies:');
-          setMovies(response.data.films);
+          const filteredMovies = response.data.films.slice(2);
+          console.log('filteredMovies:');
+          console.log(filteredMovies);
           console.log(response.data.films);
+          setMovies(filteredMovies);
           const genres = response.data.films.map(async movie => {
             const result = await getGenres(movie.imdb_title_id);
             console.log('result:');
@@ -73,7 +109,7 @@ const Dashboard = () => {
       <Text>Hello</Text>
       {movies && (
         <FlatList
-          data={movies}
+          data={movies.slice(2)}
           renderItem={({ index, item }) => {
             return (
               <MovieCard
